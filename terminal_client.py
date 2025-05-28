cbfc0b07c51aeb6a8e62050fb417b5348128fd92
lilyreber <lilyreber404@gmail.com>
2025-05-28 17:56:57 +0300

    add cli

--- terminal_client.py
+++ terminal_client.py
@@ -33,3 +33,36 @@ def send_event(event_type, data):
 
 send_event('orders', {'id': 123, 'items': ['coffee', 'cake'], 'status': 'submitted'})
 send_event('feedback', {'id': 123, 'items': ['coffee', 'cake'], 'status': 'submitted'})
+
+
+
+while True:
+    try:
+        event_type = input("Введите тип события (orders/info/feedback/exit): ").strip()
+        if event_type.lower() == 'exit':
+            break
+
+        data = []
+        if event_type == 'orders':
+            print("Для завершения ввода заказа введите 'end'")
+            item = input("Введите товар: ").strip()
+
+            while item != 'end':
+                count = int(input("Введите количество: ").strip())
+                item = input("Введите товар: ").strip()
+                data.append({item: count})
+
+        if event_type == 'feedback':
+            message = input("Введите сообщение: ").strip()
+            data.append(message)
+
+        try:
+            send_event(event_type, data)
+            print(f"Событие успешно отправлено: {event_type} с данными {data}")
+        except Exception as e:
+            print(f"Ошибка: {str(e)}")
+
+    except KeyboardInterrupt:
+        print("\nПрограмма завершена")
+        break
+
