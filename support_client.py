beea8b8ac9eac0c2628807b52ac4ec3b9c1d44a2
Igor-Stovba <stovba.igor1@mail.ru>
2025-05-28 17:26:45 +0300

    added support client

---
+++ support_client.py
@@ -0,0 +1,12 @@
+import pika, json
+from common import RABBITMQ_URL
+
+def on_order(channel, method, properties, body):
+    order = json.loads(body)
+    print(f"New order: {order['id']}")
+
+connection = pika.BlockingConnection(pika.URLParameters(RABBITMQ_URL))
+channel = connection.channel()
+
+channel.basic_consume(queue='feedback.worker', on_message_callback=on_order, auto_ack=True)
+channel.start_consuming()
